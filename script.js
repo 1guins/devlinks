e6fe07d96ef91fb0194ff6e6d14712380bd7530a
Guilherme <you@example.com>
2024-06-11 15:54:54 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.jpg")
+  }
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('alt', 'Foto de Guilherme Nunes sorrindo, dentro do carro, de camiseta branca e óculos escuro')
+  } else {
+    img.setAttribute('alt', "Foto de Guilherme Nunes sorrindo de camisa social de linho branca, barba e fundo claro"
+    )
+  }
+}
